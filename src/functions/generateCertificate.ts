c87d8d69028dc3a6c83238576251ea18d4b4126e
Paulo Vieira <paullocostta86@gmail.com>
2021-05-10 00:52:46 -0300

    fix: format code

--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -1,7 +1,7 @@
 import chromium from "chrome-aws-lambda";
 import path from "path";
 import handlebars from "handlebars";
-import fs from "fs"
+import fs from "fs";
 import { S3 } from "aws-sdk";
 
 import { document } from "../utils/dynamodbClient";
@@ -20,36 +20,52 @@ interface ITemplate {
   medal: string;
 }
 
-const compile = async function(data: ITemplate) {
-  const filePath = path
-    .join(process.cwd(), "src", "templates", "certificate.hbs");
+const compile = async function (data: ITemplate) {
+  const filePath = path.join(
+    process.cwd(),
+    "src",
+    "templates",
+    "certificate.hbs"
+  );
 
   const html = fs.readFileSync(filePath, "utf-8");
 
   return handlebars.compile(html)(data);
-}
+};
 
 export const handle = async (event) => {
   const { id, name, grade } = JSON.parse(event.body) as ICreateCertificate;
 
-  console.log("Inserir no DynamoDB os dados do Aluno");
-  await document.put({
-    TableName: "users_certificates",
-    Item: {
-      id,
-      name,
-      grade
-    }
-  }).promise();
+  const response = await document
+    .query({
+      TableName: "users_certificates",
+      KeyConditionExpression: "id = :id",
+      ExpressionAttributeValues: { ":id": id },
+    })
+    .promise();
+
+  const [userAlreadyExists] = response.Items;
+
+  if (userAlreadyExists) {
+    console.log("Inserir no DynamoDB os dados do Aluno");
+    await document
+      .put({
+        TableName: "users_certificates",
+        Item: { id, name, grade },
+      })
+      .promise();
+  }
 
   console.log("Gerar certificado");
-  const date = new Date().toLocaleDateString(
-    'pt-BR', { day: '2-digit', month: '2-digit', year: 'numeric' },
-  );
+  const date = new Date().toLocaleDateString("pt-BR", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+  });
   const medalPath = path.join(process.cwd(), "src", "templates", "selo.png");
   const medal = fs.readFileSync(medalPath, "base64");
 
-  const data: ITemplate = { id, name, grade, date, medal }
+  const data: ITemplate = { id, name, grade, date, medal };
 
   const content = await compile(data);
 
@@ -58,7 +74,7 @@ export const handle = async (event) => {
     headless: true,
     args: chromium.args,
     defaultViewport: chromium.defaultViewport,
-    executablePath: await chromium.executablePath
+    executablePath: await chromium.executablePath,
   });
 
   const page = await browser.newPage();
@@ -66,11 +82,11 @@ export const handle = async (event) => {
   await page.setContent(content);
 
   const pdf = await page.pdf({
-      format: "a4",
-      landscape: true,
-      path: process.env.IS_OFFLINE ? "certificate.pdf" : null,
-      printBackground: true,
-      preferCSSPageSize: true,
+    format: "a4",
+    landscape: true,
+    path: process.env.IS_OFFLINE ? "certificate.pdf" : null,
+    printBackground: true,
+    preferCSSPageSize: true,
   });
 
   await browser.close();
@@ -78,22 +94,24 @@ export const handle = async (event) => {
   console.log("Salvar no S3");
   const s3 = new S3();
 
-  await s3.putObject({
+  await s3
+    .putObject({
       Bucket: "sls-certificates",
       Key: `${id}.pdf`,
       ACL: "public-read",
       Body: pdf,
-      ContentType: "application/pdf"
-  }).promise();
+      ContentType: "application/pdf",
+    })
+    .promise();
 
   return {
     statusCode: 201,
     body: JSON.stringify({
       message: "Certificate created",
-      url: `https://sls-certificates.s3.amazonaws.com/${id}.pdf`
+      url: `https://sls-certificates.s3.amazonaws.com/${id}.pdf`,
     }),
     headers: {
-      "Content-Type": "application/json"
-    }
-  }
-}
+      "Content-Type": "application/json",
+    },
+  };
+};
