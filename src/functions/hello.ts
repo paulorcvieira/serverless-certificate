babe70decff14b6eaa9c06a6b60393fcc7aea925
Paulo Vieira <paullocostta86@gmail.com>
2021-05-09 22:24:52 -0300

    chore: start serverless project

---
+++ src/functions/hello.ts
@@ -0,0 +1,13 @@
+
+
+export const handle = async (event) => {
+  return {
+    statusCode: 201,
+    body: JSON.stringify({
+      message: "Hello World!"
+    }),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }
+}
